e283fd915f385e94f6768c40dc3b545286fac9db
SuisQi <160198334+SuisQi@users.noreply.github.com>
2024-07-05 10:04:27 +0800

    允许图片跨域

--- templates/map/src/heightmap/reducer.ts
+++ templates/map/src/heightmap/reducer.ts
@@ -4,7 +4,7 @@ import {UserSettingsActionType as USAT } from '../ui/types';
 import { maps } from "../common/mapData";
 import { canvas2world } from "../world/transformations";
 export let $imagedataCache = null;
-const defaultHeightmap = (): Heightmap => ({ 
+const defaultHeightmap = (): Heightmap => ({
     transform: mat4.create(),
     texture: {
       transform: mat4.create(),
@@ -25,7 +25,7 @@ const defaultHeightmap = (): Heightmap => ({
   */
 
 const transition = (state: Heightmap, mapId: keyof (typeof maps)) => {
-  
+
   const mapData = maps[mapId]
   const scale = vec3.fromValues(
     mapData.landscape.scale_x,
@@ -33,13 +33,13 @@ const transition = (state: Heightmap, mapId: keyof (typeof maps)) => {
     mapData.landscape.scale_z,
   )
   const location = vec3.fromValues(
-    mapData.landscape.loc_x, 
+    mapData.landscape.loc_x,
     mapData.landscape.loc_y,
     0
   )
   const textureOffset = vec3.fromValues(
     //@ts-ignore
-    mapData.landscape.slice_x || 0, 
+    mapData.landscape.slice_x || 0,
     //@ts-ignore
     mapData.landscape.slice_y || 0,
     0
@@ -59,17 +59,18 @@ const transition = (state: Heightmap, mapId: keyof (typeof maps)) => {
   mat4.scale(textureTransform, textureTransform, scale)//vec3.fromValues(1, 1, 1));
   // mat4.rotate.... // texture itself is not rotated relative to map object
   mat4.translate(textureTransform, textureTransform, textureOffset) // this is necessary for skorpo heightmap
-  
-  
+
+
   const texture = {...state.texture};
   texture.source = mapData.heightmap_image_src;
+  texture.image.crossOrigin="anonymous";
   texture.image.onload = () => {
     state.canvas.width = mapData.landscape.resolution_x;
     state.canvas.height = mapData.landscape.resolution_y;
     console.log("drawing hm: ", mapData.heightmap_image_src)
     const ctx = state.canvas.getContext("2d");
     ctx?.drawImage(texture.image, 0, 0);
-    
+
     $imagedataCache = ctx?.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height) as any;
     //console.log("caching imagedata for SlowFox", $imagedataCache)
   }
@@ -88,7 +89,7 @@ export const heightmap = (state: any, action: any) => {
     if (state === undefined){
         return defaultHeightmap();
     }
-    if (action.type !== USAT.write || action.payload.key !== "mapId") { 
+    if (action.type !== USAT.write || action.payload.key !== "mapId") {
         return state;
     }
     return transition(state, action.payload.value)
